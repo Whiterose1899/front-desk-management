952085968402f074563023ee367766b46163d8bc
Karthikesh Bangi <bangikarthikesh91@gmail.com>
2026-07-03 23:59:20 +0530

    CORS error in main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://front-desk-management.onrender.com",
+        "https://fdms-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
